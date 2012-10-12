aa64684ac00fb2177941ffd73353e6f98bb014e5
Homer Strong <homer@luckysort.com>
2012-10-12 10:52:44 -0700

    use mallet jar from clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject chisel "1.0.0-SNAPSHOT"
   :description "Do LDA topic modeling using MALLET via the Java API"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [edu.umass/mallet "0.0.1-SNAPSHOT"]
+                 [org.clojars.strongh/mallet "2.0.7-SNAPSHOT"]
                  [net.sf.trove4j/trove4j "2.0.2"]]
   :dev-dependencies [[swank-clojure "1.3.1"]
                      [marginalia "0.3.2"]])
