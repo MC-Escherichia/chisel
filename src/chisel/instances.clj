0fb3575fe8a92815a10e1e6a8aa36d8e0fb1e8a1
Homer Strong <homer@luckysort.com>
2012-10-12 10:53:31 -0700

    build models from tokens

--- src/chisel/instances.clj
+++ src/chisel/instances.clj
@@ -5,13 +5,19 @@
   (:import (cc.mallet.pipe Pipe
                            SerialPipes
                            CharSequence2TokenSequence
-                           TokenSequence2FeatureSequence)))
+                           TokenSequence2FeatureSequence
+                           StringList2FeatureSequence)))
                                   
 (defn document-to-instance
   "Convert a document ID and a text string to a MALLET instance"
   [[documentid text]]
   (Instance. text "nolabel" documentid nil))
-                           
+
+(defn tokens-to-instance
+  "Convert a document ID and a text string to a MALLET instance"
+  [[documentid text]]
+  (Instance. (java.util.ArrayList. text) "nolabel" documentid nil))
+
 (defn get-instance-list
   "Convert (documentID, text) map to MALLET InstanceList"
   [documentmap]
@@ -29,3 +35,10 @@
                     (new TokenSequence2FeatureSequence)])]                    
     (doto (new InstanceList pipes)
       (.addThruPipe (.iterator instanceiter)))))
+
+(defn get-instance-list-from-tokens
+  "Convert (documentID, [\"tokens\"]) map to MALLET InstanceList"
+  [documentmap]
+  (let [pipe (new StringList2FeatureSequence)]
+    (doto (new InstanceList pipe)
+      (.addThruPipe (.iterator (map tokens-to-instance documentmap))))))
